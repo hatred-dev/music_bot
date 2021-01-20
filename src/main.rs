9474373eac207ec7cbe364ff35412b75e24784c5
Edward Zelis <zeledvards@gmail.com>
2021-01-20 06:23:11 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -505,25 +505,25 @@ async fn stop(ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
     let manager = songbird::get(ctx)
         .await
         .expect("Songbird Voice client placed in at initialisation.");
+    let has_handler = manager.get(guild_id).is_some();
+    if has_handler {
+        if let Err(e) = manager.remove(guild_id).await {
+            check_msg(
+                msg.channel_id
+                    .say(&ctx.http, format!("Failed: {:?}", e))
+                    .await,
+            );
+        }
 
+        check_msg(msg.channel_id.say(&ctx.http, "Left voice channel").await);
+    } else {
+        check_msg(msg.reply(ctx, "Not in a voice channel").await);
+    }
     if let Some(handler_lock) = manager.get(guild_id) {
         let handler = handler_lock.lock().await;
         let queue = handler.queue();
         let _ = queue.stop();
-        let has_handler = manager.get(guild_id).is_some();
-        if has_handler {
-            if let Err(e) = manager.remove(guild_id).await {
-                check_msg(
-                    msg.channel_id
-                        .say(&ctx.http, format!("Failed: {:?}", e))
-                        .await,
-                );
-            }
 
-            check_msg(msg.channel_id.say(&ctx.http, "Left voice channel").await);
-        } else {
-            check_msg(msg.reply(ctx, "Not in a voice channel").await);
-        }
         check_msg(msg.channel_id.say(&ctx.http, "Queue cleared.").await);
     } else {
         check_msg(
