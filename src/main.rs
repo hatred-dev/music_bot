2f7abf921613cb0d0b6d7e07cd4ac33f2557d1e9
Edward Zelis <zeledvards@gmail.com>
2021-02-05 00:20:37 +0200

    automation

--- src/main.rs
+++ src/main.rs
@@ -227,7 +227,23 @@ async fn play(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
 
     let guild = msg.guild(&ctx.cache).await.unwrap();
     let guild_id = guild.id;
+
+    let channel_id = guild
+        .voice_states
+        .get(&msg.author.id)
+        .and_then(|voice_state| voice_state.channel_id);
+
+    let connect_to = match channel_id {
+        Some(channel) => channel,
+        None => {
+            check_msg(msg.reply(ctx, "Not in a voice channel").await);
+
+            return Ok(());
+        }
+    };
+
     let manager = songbird::get(ctx).await.expect("Songbird Voice client placed in at initialisation.").clone();
+    let _handler = manager.join(guild_id,connect_to).await;
     if let Some(handler_lock) = manager.get(guild_id) {
         let mut handler = handler_lock.lock().await;
         unsafe {
